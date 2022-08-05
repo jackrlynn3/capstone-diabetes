701d1f567ce08ee753ea2aec3f6888a258ef129c
jackrlynn3 <jackrlynn@gmail.com>
2022-08-05 09:38:26 -0500

    Added ERD diagram

--- datasets/database-formation.sql
+++ datasets/database-formation.sql
@@ -3,6 +3,10 @@ USING group5database;
 GO
 
 -- Drop exisiting versions of the database
+DROP TABLE IF EXISTS CensusStat;
+DROP TABLE IF EXISTS [State];
+DROP TABLE IF EXISTS Metric;
+DROP TABLE IF EXISTS GlucoseMeter;
 DROP TABLE IF EXISTS DiabetesPop;
 DROP TABLE IF EXISTS Demographic;
 GO
@@ -42,16 +46,45 @@ CREATE TABLE DiabetesPop (
     famhistID INT NOT NULL,
     CONSTRAINT u_personID
         UNIQUE (personID),
-    CONSTRAINT fk_sexID_Demographic
-        FOREIGN KEY (sexID)
-        REFERENCES Demographic(demoID),
-    CONSTRAINT fk_smokerID_Demographic
-        FOREIGN KEY (smokerID)
-        REFERENCES Demographic(demoID),
-    CONSTRAINT fk_drinkerID_Demographic
-        FOREIGN KEY (drinkerID)
-        REFERENCES Demographic(demoID),
-    CONSTRAINT fk_famhistID_Demographic
-        FOREIGN KEY (famhistID)
-        REFERENCES Demographic(demoID)
+);
+
+-- Create GlucoseMeter table
+CREATE TABLE GlucoseMeter (
+    recID INT PRIMARY KEY NOT NULL, -- PK
+    [time] VARCHAR(30) NOT NULL,
+    glucose_lvl INT NOT NULL,
+    ptID INT NOT NULL, -- FK
+    CONSTRAINT u_recID
+        UNIQUE(recID)
+);
+
+-- Create State table
+
+CREATE TABLE [State] (
+    stateID varchar(2) PRIMARY KEY NOT NULL, -- PK
+    [name] varchar(20) NOT NULL,
+    CONSTRAINT u_stateID
+        UNIQUE (stateID)
+);
+
+-- Create Metric table
+
+CREATE TABLE [Metric] (
+    metricID INT PRIMARY KEY NOT NULL, -- PK
+    metric VARCHAR(30) NOT NULL,
+    unit VARCHAR(10) NULL,
+    CONSTRAINT u_metricID
+        UNIQUE (metricID),
+    CONSTRAINT u_metric
+        UNIQUE (metric)
+);
+
+-- Create CensusStat table
+
+CREATE TABLE CensusStat (
+    stateID varchar(2) NOT NULL, -- PK
+    metricID INT NOT NULL, -- PK
+    demoID INT NOT NULL, -- PK
+    [year] INT NOT NULL,
+    [value] DECIMAL(5, 2)
 );
