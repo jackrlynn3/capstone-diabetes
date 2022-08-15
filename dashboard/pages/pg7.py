00466de993f2c4f3cccc717853a534fc5a2ea615
jackrlynn3 <jackrlynn@gmail.com>
2022-08-15 16:02:49 -0500

    Added implementation for optimal model and training time graphs

--- dashboard/pages/pg7.py
+++ dashboard/pages/pg7.py
@@ -10,7 +10,7 @@ import pymssql
 dash.register_page(__name__, name='DL Prediction')
 
 # Create training_card
-def createTrainingCard(header_md, card_images_links, card_image_titles, card_image_parts, analysis_md):
+def createTrainingCard(header_md, card_images_links, card_image_titles, card_image_parts, analysis_md, overwrite=False):
     training_card = []
 
     # Fill in header
@@ -19,7 +19,8 @@ def createTrainingCard(header_md, card_images_links, card_image_titles, card_ima
 
     # Create cards
     cards_contents = []
-    for i in range(len(card_images_links)):
+    num_cards = len(card_images_links)
+    for i in range(num_cards):
 
         # Get components
         image = card_images_links[i]
@@ -27,8 +28,11 @@ def createTrainingCard(header_md, card_images_links, card_image_titles, card_ima
         parts = card_image_parts[i]
 
         # For the card
+        width = "100%"
+        if (overwrite):
+            width = "40%"
         this_card = dbc.Col([dbc.Card([dbc.CardImg(src=image, top=True),
-            dbc.CardBody([dcc.Markdown(title)])], style={"width": "100%"})],
+            dbc.CardBody([dcc.Markdown(title)])], style={"width": width})],
             style={"flex-grow": str(parts)}
         ) 
 
@@ -55,7 +59,7 @@ header_delay = '''
 '''
 images_delay = ["https://github.com/jackrlynn3/capstone-diabetes/blob/main/visualizations/deep-learning/time-delay-2.png?raw=true",
     "https://raw.githubusercontent.com/jackrlynn3/capstone-diabetes/main/visualizations/deep-learning/time-delay-1.png?raw=true"]
-titles_delay = ["#### Evaluation of Time Delay Models", "#### Comparison of Time Delay Models to Truth"]
+titles_delay = ["#### **Evaluation of Time Delay Models**", "#### **Comparison of Time Delay Models to Truth**"]
 parts_delay = ['1', '2']
 analysis_delay = '''
     #### **Evaluation**
@@ -67,15 +71,15 @@ analysis_delay = '''
 '''
 delay_card = createTrainingCard(header_delay, images_delay, titles_delay, parts_delay, analysis_delay)
 
-# width test
+# Width test
 header_window = '''
-    Window Sizes describes how many previous inputs are considered for the current estimation (e.g., using
+    Window sizes describes how many previous inputs are considered for the current estimation (e.g., using
     the 10 previous inputs for the current estimation). Window sizes are varied by 5 previous points, ranging
     from 5 to 50 previous timepoints.
 '''
 images_window = ["https://github.com/jackrlynn3/capstone-diabetes/blob/main/visualizations/deep-learning/width-1.png?raw=true",
     "https://github.com/jackrlynn3/capstone-diabetes/blob/main/visualizations/deep-learning/width-2.png?raw=true"]
-titles_window = ["#### Evaluation of Window Size Models", "#### Comparison of Window Size Models to Truth"]
+titles_window = ["#### **Evaluation of Window Size Models**", "#### **Comparison of Window Size Models to Truth**"]
 parts_window = ['1', '2']
 analysis_window = '''
     #### **Evaluation**
@@ -83,7 +87,43 @@ analysis_window = '''
     increasing window size. A window size of 20 previous points is used to maximize accuracy without too much
     computational cost.
 '''
-window_card = createTrainingCard(header_window, images_window, titles_window, parts_window, analysis_window) 
+window_card = createTrainingCard(header_window, images_window, titles_window, parts_window, analysis_window)
+
+# Training test
+header_train = '''
+    Training time describes how many training epochs are required for the model to be sufficiently trained.
+    Training times are varied between 1 and 50 to determine how much training is required to minimize loss.
+'''
+images_train = ["https://github.com/jackrlynn3/capstone-diabetes/blob/main/visualizations/deep-learning/training-time-1.png?raw=true"]
+titles_train = ["#### **Evaluation of Training Time Models**"]
+parts_train = ['1']
+analysis_train = '''
+    #### **Evaluation**
+    Loss rounds off at around 6 epochs, though some improvements occur up until 30. Training time linearly increases
+    with the number of epochs. The ideal number of training epochs chosen is 30 to balance both training time and
+    loss reduction.
+'''
+train_card = createTrainingCard(header_train, images_train, titles_train, parts_train, analysis_train, overwrite=True)
+
+# Optimal model
+header_optimal = '''
+    The optimal model is trained to have the following parameters:
+    1. Prediction 30 min into future
+    2. Window size of 20
+    3. 30 training epochs
+    4. 1 LSTM layer; 1 GRU layer
+'''
+images_optimal = ["https://github.com/jackrlynn3/capstone-diabetes/blob/main/visualizations/deep-learning/optimum-model-1.png?raw=true",
+    "https://github.com/jackrlynn3/capstone-diabetes/blob/main/visualizations/deep-learning/optimum-model-2.png?raw=true"]
+titles_optimal = ["#### **Evaluation of Optimal Model over 30 Training Epochs**",
+    "#### **Comparison of Optimal Model to Truth**"]
+parts_optimal = ['1', '2']
+analysis_optimal = '''
+    #### **Evaluation**
+    This model took 299 sec to train and has a result mean squared error loss of 1337. Note that this model is not
+    an absolute minimization of loss but rather a balance of training time, loss, and predictive power into the future.
+'''
+optimal_card = createTrainingCard(header_optimal, images_optimal, titles_optimal, parts_optimal, analysis_optimal)
 
 layout = html.Div(
     [
@@ -127,7 +167,8 @@ layout = html.Div(
 
         dbc.Row(
             [
-                dcc.Dropdown(['Window Size', 'Time Delay'], 'Window Size', id="select-opt")
+                dcc.Dropdown(['Optimal Model', 'Window Size', 'Time Delay', 'Training Time'],
+                'Optimal Model', id="select-opt")
             ],
             style={'fontSize':28, 'textAlign':'left', 'font-weight':'bold'}
         ),
@@ -146,3 +187,7 @@ def show_graph(opt_select):
         return window_card
     elif (opt_select == 'Time Delay'):
         return delay_card
+    elif (opt_select == 'Training Time'):
+        return train_card
+    elif (opt_select == 'Optimal Model'):
+        return optimal_card
