3fe5033f4ed53742614b5a18a4bec35ddda5478b
wainrightsamantha <swainright@dev-10.com>
2022-08-16 14:34:50 -0600

    Update pg4.py

--- dashboard/pages/pg4.py
+++ dashboard/pages/pg4.py
@@ -5,15 +5,14 @@ from dash import Dash, dcc, html, Input, Output
 from dash import dcc, html, callback, Output, Input
 import plotly.express as px
 import dash_bootstrap_components as dbc
-
 # data processing
 import pandas as pd
 from dash import html, dcc
 import pymssql
-
 # model and performance
 from sklearn.feature_selection import SelectKBest, f_classif
 from sklearn.model_selection import train_test_split, cross_val_score, RepeatedStratifiedKFold
+
 
 dash.register_page(__name__, name='Predicting Diabetes')
 
@@ -45,52 +44,44 @@ def anova_select_features(X_train, y_train, X_test):
     
     return X_train_fs, X_test_fs, afs
 
+#############################################################
+####################### PAGE LAYOUT #########################
 layout = html.Div(
     [
-        dbc.Row(
-            [
+
+        dbc.Row([
                 dcc.Markdown('''## Can We Predict Diabetes''')
-            ]
-        ),
-        dbc.Row(
-            [
+            ]),
+
+        dbc.Row([
                 dbc.Col(dcc.Markdown('''
                     Explain the Model
 
                     things
 
-                    stuff
-                    ''')
-                ),
+                    stuff!!
+                    ''')),
+
                 dbc.Col(
-                    html.Img(src='https://raw.githubusercontent.com/jackrlynn3/capstone-diabetes/main/visualizations/machine-learning/CM_Tuned_SF_LSVC.png')
-                ),
+                    html.Img(src='https://raw.githubusercontent.com/jackrlynn3/capstone-diabetes/main/visualizations/machine-learning/CM_Tuned_SF_LSVC.png')),
+
                 dbc.Col(
-                    html.Img(src='https://raw.githubusercontent.com/jackrlynn3/capstone-diabetes/main/visualizations/machine-learning/CM_Tuned_SF_SVC.png')
-                ),
+                    html.Img(src='https://raw.githubusercontent.com/jackrlynn3/capstone-diabetes/main/visualizations/machine-learning/CM_Tuned_SF_SVC.png')),
+
                 html.Div(html.Br())
-            ],
-        ),
-        dbc.Row(
-            [
-                dbc.Col(
-                    [
+            ],),
+
+        dbc.Row([
+                dbc.Col([
                     (html.Img(
-                        src=('https://raw.githubusercontent.com/jackrlynn3/capstone-diabetes/main/visualizations/machine-learning/SF_anova_scores.png'),
-                        style={
-                            'width':'10%',
-                            'height':'10%'
-                            }
-                            )
+                        src=('https://raw.githubusercontent.com/jackrlynn3/capstone-diabetes/main/visualizations/machine-learning/SF_anova_scores.png'),)
                     )
-                    ],
-                ),
+                    ],width=6),
+
                 dbc.Col(
                     [
                     html.Img(src='https://raw.githubusercontent.com/jackrlynn3/capstone-diabetes/main/visualizations/machine-learning/ROC_AUC_LSVC.png')
-                    ], width=6
-                ),
-            ],
-        ),
+                    ], width=6),
+            ],),
     ]
 )
