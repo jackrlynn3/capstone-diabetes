3564d136161340f6e63866688479fcc64867d406
HaydenKMuscha <HMuscha@dev-10.com>
2022-08-15 23:54:39 -0500

    Rough outline of ML page.

--- dashboard/pages/pg4.py
+++ dashboard/pages/pg4.py
@@ -1,10 +1,52 @@
 import dash
-from dash import dcc, html
-
+from dash import Dash, dcc, html, Input, Output
+from dash import dcc, html, callback, Output, Input
+import plotly.express as px
+import dash_bootstrap_components as dbc
+import pandas as pd
+from dash import html, dcc
+import pymssql
 dash.register_page(__name__, name='Predicting Diabetes')
 
 layout = html.Div(
     [
-        dcc.Markdown('Can We Predict Diabetes?')
+        dbc.Row(
+            [
+                dcc.Markdown('''##### Can We Predict Diabetes''')
+            ]
+        ),
+        dbc.Row(
+            [
+                dbc.Col(dcc.Markdown('''
+                    Explain the Model
+
+                    things
+
+                    stuff
+                    ''')
+                ),
+                dbc.Col(
+                    html.Img(src='https://raw.githubusercontent.com/jackrlynn3/capstone-diabetes/main/visualizations/machine-learning/CM_Tuned_SF_LSVC.png')
+                ),
+                dbc.Col(
+                    html.Img(src='https://raw.githubusercontent.com/jackrlynn3/capstone-diabetes/main/visualizations/machine-learning/CM_Tuned_SF_SVC.png')
+                ),
+                html.Div(html.Br())
+            ],
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                    (html.Img(src=('https://raw.githubusercontent.com/jackrlynn3/capstone-diabetes/main/visualizations/machine-learning/SF_anova_scores.png'),style={'height':'75%'}))
+                    ], width = 6
+                ),
+                dbc.Col(
+                    [
+                    html.Img(src='https://raw.githubusercontent.com/jackrlynn3/capstone-diabetes/main/visualizations/machine-learning/ROC_AUC_LSVC.png')
+                    ], width=6
+                ),
+            ],
+        ),
     ]
 )
