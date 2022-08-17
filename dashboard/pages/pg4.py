0929f2b608126bb2dad19b1bd5901b3d1fa7fbbb
wainrightsamantha <swainright@dev-10.com>
2022-08-16 23:29:31 -0600

    features plot

--- dashboard/pages/pg4.py
+++ dashboard/pages/pg4.py
@@ -1,32 +1,19 @@
-import os
-# ML libraries
-import joblib
-from joblib import load
-import pickle
-import pandas as pd
-
-# Plotly and Dash libraries
 import dash
 from dash import Dash, dcc, html, Input, Output
 from dash import dcc, html, callback, Output, Input
 import plotly.express as px
 import dash_bootstrap_components as dbc
+import pandas as pd
+from dash import html, dcc
 import pymssql
 
-# os.chdir('../..')
-# home_path = os.getcwd()
-
-# models_path = os.path.join(home_path,'models/machine-learning/models')
-# models_path
-
-#anova selected features
-# selected_featuresDF = pd.read_csv('https://raw.githubusercontent.com/jackrlynn3/capstone-diabetes/main/models/machine-learning/models/sorted_features.csv')
-# df = selected_featuresDF.sort_values(by='Anova_Score', ascending=True)
-# fig = px.bar(df, x='Anova_Score',y='Feature')
-# fig.update_layout(title="Anova Score of Features",
-#                 yaxis_title=None)
-
 dash.register_page(__name__, name='Predicting Diabetes')
+
+# feature selection bar graph
+selected_featuresDF = pd.read_csv('https://raw.githubusercontent.com/jackrlynn3/capstone-diabetes/main/models/machine-learning/models/sorted_features.csv')
+df = selected_featuresDF.sort_values(by='Anova_Score', ascending=True)
+fig = px.bar(df, x='Anova_Score',y='Feature',title="Anova Score of Features")
+fig.update_layout(title_x=0.5,yaxis_title=None)
 
 layout = html.Div(
     [
@@ -35,6 +22,7 @@ layout = html.Div(
                 dcc.Markdown('''##### Can We Predict Diabetes''')
             ]
         ),
+
         dbc.Row(
             [
                 dbc.Col(dcc.Markdown('''
@@ -52,12 +40,14 @@ layout = html.Div(
                 html.Div(html.Br())
             ],
         ),
+
         dbc.Row(
             [
                 dbc.Col(
                     [
-                    html.Img(src='https://raw.githubusercontent.com/jackrlynn3/capstone-diabetes/main/visualizations/machine-learning/ROC_AUC_LSVC.png')
-                    ], width=4
+                    dcc.Graph(id='feature_selection',
+                    figure=fig)
+                    ], width = 6
                 ),
                 dbc.Col(
                     [
@@ -66,20 +56,6 @@ layout = html.Div(
                 ),
             ],
         ),
+
     ]
 )
-
-# @ callback(
-#     Output(component_id='selected_features',
-#     component_property='figure'),
-#     [Input(component_id)]
-    
-#     )
-
-                # dbc.Col(
-                #     [
-                #     dcc.Graph(
-                #         id='selected_features',
-                #         figure=fig),
-                #     ], width=4
-                # ),
