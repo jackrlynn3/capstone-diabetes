0bfc0e57fa3c16f229c294594ae2d9381ca48ebc
wainrightsamantha <swainright@dev-10.com>
2022-08-17 21:40:04 -0600

    finished pg2

--- dashboard/pages/pg2.py
+++ dashboard/pages/pg2.py
@@ -18,7 +18,7 @@ try:
 except:
     selected_featuresDF = pd.read_csv('dashboard/assets/sorted_features.csv')
 df = selected_featuresDF.sort_values(by='Anova_Score', ascending=True)
-fig = px.bar(df, x='Anova_Score',y='Feature',title="Anova Score of Features",color_discrete_sequence=['Teal']*len(df))
+fig = px.bar(df, x='Anova_Score',y='Feature',title="Predictive Features",color_discrete_sequence=['Teal']*len(df))
 fig.update_layout(title_x=0.5,
                 yaxis_title=None,
                 xaxis_title='Anova Score',
@@ -31,38 +31,42 @@ layout = html.Div(
     [
         dbc.Row(
             [
-                dcc.Markdown('''##### Can We Predict Diabetes''')
+                dcc.Markdown('''##### Can We Predict Diabetes?''')
             ]
         ),
 
         dbc.Row(
             [
                 dbc.Col(dcc.Markdown('''
-                    Explain the Model
-                    things
-                    stuff
+                        The ability to predict a Type-2 diabetes diagnosis can be done with machine learning.
+                        Type 2 diabetes is mainly lifestyle related and develops overtime. When using an ANOVA F-test, on the Chen et al. data we see that the strongest predictive features are age, bmi, blood pressure, and cholesterol.
+
+                        And ROC Curve and confusion matrix represent the performance of the LinearSVC algorithm, as well as it's ability to correctly predict diabetes. 
+
                     ''')
                 ),
-                dbc.Col(
-                    [
-                    dcc.Graph(id='feature_selection',
-                    figure=fig)
-                    ], width = 6
-                ),
-                html.Div(html.Br())
             ],
         ),
 
         dbc.Row(
             [
                 dbc.Col(
+                    [
+                    dcc.Graph(id='feature_selection',
+                    figure=fig)
+                    ], width=4
+                ),
+                dbc.Col(
+                    [
                     html.Img(src='https://raw.githubusercontent.com/jackrlynn3/capstone-diabetes/main/visualizations/machine-learning/LSVCConfusionMatrix.png')
+                    ], width=4
                 ),
                 dbc.Col(
                     [
                     html.Img(src='https://raw.githubusercontent.com/jackrlynn3/capstone-diabetes/main/visualizations/machine-learning/ROCcurveLSVC.png')
-                    ], width=6
+                    ], width=4
                 ),
+                html.Div(html.Br())
             ],
         ),
 
